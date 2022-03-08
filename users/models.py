4415fe43ba4b04234e063141eaeef10c822e84eb
Basit Ansari <basit.ansari@somaiya.edu>
2022-03-08 20:08:00 +0530

    changes in users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    aadhar = models.CharField(max_length=12, blank=False,null=True)
+    phone = models.CharField(max_length=10, blank=False,null=True)
