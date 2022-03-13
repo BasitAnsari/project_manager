f8d6007d19d8f60b062a515415d22fce8e014dcc
Basit Ansari <basit.ansari@somaiya.edu>
2022-03-13 22:21:48 +0530

    halfway through auth

--- users/models.py
+++ users/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    aadhar = models.CharField(max_length=12, blank=False,null=True)
-    phone = models.CharField(max_length=10, blank=False,null=True)
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE,db_constraint=False)
+    stu_id = models.IntegerField(null=True)
+    mob_num = models.IntegerField(null=True)
+    stu_col = models.TextField(null=True)
+    
--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+import profile
+from django.shortcuts import redirect, render
+from users.models import Profile
 # Create your views here.
+def login_view(request):
+    return render(request, "Registration/login.html")
+
+def signup_view(request):
+    if request.method == "POST":
+        profile = Profile()
+        profile.username = request.POST.get('username')
+        profile.first_name = request.POST.get('first_name')
+        profile.last_name = request.POST.get('last_name')
+        profile.email = request.POST.get('email')
+        profile.password = request.POST.get('password')
+        profile.stu_id = request.POST.get('stu_id')
+        profile.stu_col = request.POST.get('stu_col')
+        profile.mob_num = request.POST.get('mob_num')
+        profile.save()
+        
+        return redirect('home')
+    return render(request, "Registration/signup.html")
