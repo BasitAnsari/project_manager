e9bcfe529ff5e18e433b5f9beec36205a2bde37b
Prat <prathamesh8more@gmail.com>
2022-03-18 20:28:46 +0530

    More change

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from multiprocessing import context
 from users.models import User, Profile
+from project.models import Project
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-from .forms import UserRegisterForm,ProfileForm
+from .forms import UserRegisterForm, ProfileForm
 # Create your views here.
+
+
 def signup_view(request):
     if request.method == "POST":
         u_form = UserRegisterForm(request.POST)
@@ -20,6 +23,7 @@ def signup_view(request):
         p_form = ProfileForm()
     return render(request, "Registration/signup.html", {'u_form': u_form, 'p_form': p_form})
 
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -30,19 +34,26 @@ def login_view(request):
             return redirect('home')
         else:
             context = {
-                "error" : "Invalid Username or Password"
+                "error": "Invalid Username or Password"
             }
-            return render(request, "Registration/login.html",context)
+            return render(request, "Registration/login.html", context)
     return render(request, "Registration/login.html")
+
+
 @login_required(login_url='login/')
 def profile_view(request):
-    profileobj = Profile.objects.get(user= request.user)
-    userobj = User.objects.get(id= profileobj.user_id)
+    profileobj = Profile.objects.get(user=request.user)
+    userobj = User.objects.get(id=profileobj.user_id)
+    # projobj = Project.objects.get(user=profileobj.user_id)
+    projobj = Project.objects.all()
+
     context = {
-        'user' : userobj,
+        'user': userobj,
         'profile': profileobj,
+        'projects': projobj,
     }
     return render(request, 'Registration/profile.html', context)
+
 
 @login_required(login_url='login/')
 def logout_view(request):
