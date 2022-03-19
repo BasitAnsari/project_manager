9ad1dc9c4fea212738d52e71221a6b829548c12a
Prat <prathamesh8more@gmail.com>
2022-03-19 23:36:10 +0530

    plagarism percent

--- project/models.py
+++ project/models.py
@@ -3,24 +3,26 @@ from django.db import models
 from django.contrib.auth.models import User
 
 Category_CHOICES = (
-        (None,'Select Category'),
-        ('Ongoing','Ongoing'),
-        ('Complete','Complete'),
-        ('Incomplete','Incomplete'),
-        ('Research','Research'),
-        )
+    (None, 'Select Category'),
+    ('Ongoing', 'Ongoing'),
+    ('Complete', 'Complete'),
+    ('Incomplete', 'Incomplete'),
+    ('Research', 'Research'),
+)
+
+
 class Project(models.Model):
-    title = models.CharField(max_length= 128)
+    title = models.CharField(max_length=128)
     statement = models.CharField(max_length=256)
-    domain = models.CharField(max_length= 128)
-    category = models.CharField(max_length= 128,choices=Category_CHOICES)
-    institute = models.CharField(max_length= 256,default=NULL)
+    domain = models.CharField(max_length=128)
+    category = models.CharField(max_length=128, choices=Category_CHOICES)
+    institute = models.CharField(max_length=256, default=NULL)
     is_approved = models.BooleanField(default=False)
-    #user info
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
-    
-    
+    plagarised = models.FloatField(null=True)
+    plagarised_with = models.CharField(max_length=128, blank=True, null=True)
+    # user info
+    user = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.CASCADE)
+
     def get_absolute_url(self):
         return f"/project-detail/{self.id}/"
-    
-    
--- project/views.py
+++ project/views.py
@@ -3,7 +3,29 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from .models import Project
 from .forms import ProjectForm
+from numpy import vectorize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def vectorize(text): return TfidfVectorizer().fit_transform(text).toarray()
+
+
+def similarity(text1, text2): return cosine_similarity([text1, text2])
+
+
+def check_plagarism(name, texts):
+    vectors = vectorize(texts)
+    result = set()
+    sim_score = similarity(vectors[0], vectors[1])[0][1]
+    name_pair = (name[0], name[1])
+    score = name_pair[0], name_pair[1], sim_score
+    result.add(score)
+    return result
+
 # Create your views here.
+
+
 @login_required
 def project_create_view(request):
     form = ProjectForm()
@@ -16,41 +38,66 @@ def project_create_view(request):
             instance.save()
             return redirect('home')
     context = {
-        'form' : form
-        }
-    return render(request, "Project/project_form.html",context)
+        'form': form
+    }
+    return render(request, "Project/project_form.html", context)
+
+
 @login_required
-def project_detail(request,pk):
-    obj = Project.objects.get(id= pk)
+def project_detail(request, pk):
+    obj = Project.objects.get(id=pk)
     if request.method == 'POST':
         value = request.POST.get('value')
-        if value == 'Disapprove' :
+        if value == 'Disapprove':
             obj.is_approved = False
-        else :
+        else:
             obj.is_approved = True
-        obj.save()
+    if obj.plagarised is None:
+        domain = obj.domain
+        obj2 = Project.objects.filter(domain=domain)
+
+        results = []
+        for i in obj2:
+            if not i.id == obj.id:
+                name = [obj.title, i.title]
+                texts = [obj.statement, i.statement]
+                p = list(check_plagarism(name, texts))[0]
+                pn = float(p[2])*100
+                pn = round(pn, 2)
+                print(pn)
+                results.append((p[1], pn))
+        print(results)
+        big = 0
+        for title, num in results:
+            if num > big:
+                big = num
+        plag = max(results, default=0)
+        obj.plagarised = plag
+        # obj.plagarised_with = ptitle
+    obj.save()
     context = {
         'object': obj
     }
-    return render(request,"Project/project_details.html", context)
-        
+    return render(request, "Project/project_details.html", context)
+
+
 def project_search(request):
     query = request.session['query']
     if query is not None:
-        qs = Project.objects.filter(title__icontains= query, is_approved= True)
-    else :
-        qs = Project.objects.filter(is_approved= True)
+        qs = Project.objects.filter(title__icontains=query, is_approved=True)
+    else:
+        qs = Project.objects.filter(is_approved=True)
     if request.method == 'POST':
         title = request.POST.get('title')
         domain = request.POST.get('domain')
         category = request.POST.get('category')
-        if title is not None :
-            qs = qs.filter(title__icontains= title)
-        if domain is not None :
-            qs = qs.filter(domain__icontains= domain)
-        if category is not None :
-            qs = qs.filter(category__icontains= category)
+        if title is not None:
+            qs = qs.filter(title__icontains=title)
+        if domain is not None:
+            qs = qs.filter(domain__icontains=domain)
+        if category is not None:
+            qs = qs.filter(category__icontains=category)
     context = {
-        'qs' : qs
+        'qs': qs
     }
-    return render(request, 'Project/project_list.html',context)
+    return render(request, 'Project/project_list.html', context)
